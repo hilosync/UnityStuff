cea7d261b7235c1723bec5924a3c5a1b7987f1fc
hilosync <62771262+hilosync@users.noreply.github.com>
2021-07-06 16:57:19 +0100

    Dragon Block Breaker Initial Commit

--- repos/2d unity/Number Wizard UI/Assets/Scripts/NumberWizard.cs
+++ repos/2d unity/Number Wizard UI/Assets/Scripts/NumberWizard.cs
@@ -1,38 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class NumberWizard : MonoBehaviour
 {
     [SerializeField] int max;
     [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
     int guess;
     // Start is called before the first frame update
     void Start()
     {
         StartGame();
     }
+    void NextGuess()
+    {
+        guess = Random.Range(min,max+1);
+        guessText.text = guess.ToString();
+    }
 
     void StartGame()
     {
-        max++;
-        guess = (max + min) /2;
-        
+        NextGuess();
     }
 
     public void OnPressHigher()
     {
-        min = guess;
+        min = guess+1;
         NextGuess();
     }
     public void OnPressLower()
     {
-        max = guess;
+        max = guess-1;
         NextGuess();
     }
 
-    void NextGuess()
-    {
-        guess = (max + min) /2;
-    }
+
 }
